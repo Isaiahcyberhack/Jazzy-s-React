ebd954a25619cc971529fc3552ecb322d0022c0a
Isaiahcyberhack <Odoekwuisaiah@gmail.com>
2026-04-29 08:55:16 +0100

    Assign-project

--- src/assets/Components/Project/Assgn.tsx
+++ src/assets/Components/Project/Assgn.tsx
@@ -1,4 +1,7 @@
 import "./Assign.css";
+import logo from "../../Jane-miller.png";
+import jazzy from "../../hero.png";
+// import Bgimage from "../../Jane-miller.png";
 
 const Assgn = () => {
   return (
@@ -6,6 +9,37 @@ const Assgn = () => {
       <p className="Assign_h1">
         I'm a TEch Dev., Full stack Dev, cyber-Sec. Offsec
       </p>
+      <img src={logo} className="" />
+      <img src={jazzy} alt="" className="" />
+
+      <div className="bg-img" title="about_img_">
+        <h2>Hello Nigeria with About Background</h2>
+      </div>
+
+      <div className="background_two" title="Hero_img, Css style"></div>
+      <details>
+        <h4>this is an External Background styling</h4>
+      </details>
+      {/* this is another inline styling inside a div */}
+      <div
+        style={{
+          // backgroundImage: url(${Bgimage}),
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          height: "60vh",
+        }}
+      >
+        <h4
+          style={{
+            color: "red",
+            fontWeight: "bolder",
+
+            fontFamily: "sans-serif serif ,monospace",
+          }}
+        >
+          THis is an inline styling
+        </h4>
+      </div>
     </div>
   );
 };
